054477d449cb41de7627b4e1987cb14333a20932
ellion <rubcinskija@gmail.com>
2023-05-27 01:56:16 +0700

    upd

--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,4 @@
 from .users import UserService, user_service
+from .smtp import email_service
 
-__all__ = ["UserService", "user_service"]
+__all__ = ["UserService", "user_service", "email_service"]
--- trash files/emailwork.py
+++ trash files/emailwork.py
@@ -1,30 +1,30 @@
-import smtplib
-from random import randint
-from config import smtp_password
-
-email = 'service.ellion23@internet.ru'
-code = f"{randint(1, 9999):04d}"
-smtp_server = 'smtp.mail.ru:465'
-
-
-dest_email = 'rubcinskija@gmail.com'
-subject = 'Restore password'
-body = 'Use that code: ' + code + " to restore your password."
-
-letter = f"""\
-From: {email}
-To: {dest_email}
-Subject: {subject}
-Content-Type: text/plain; charset="UTF-8";
-{body}"""
-letter = letter.encode("UTF-8")
-
-
-server = smtplib.SMTP_SSL(smtp_server)
-server.login(email, smtp_password)
-server.sendmail(email, dest_email, letter)
-server.quit()
-
-class EmailService:
-    def __init__(self):
-        pass
+# import smtplib
+# from pydantic import EmailStr
+#
+# from config import smtp_password
+#
+# email = 'service.ellion23@internet.ru'
+# smtp_server = 'smtp.mail.ru:465'
+#
+#
+# class EmailService:
+#     def __init__(self):
+#         self.server = smtplib.SMTP_SSL(smtp_server)
+#
+#     def send_restore_code(self, dest_email: EmailStr, code: str):
+#         subject = 'Restore password'
+#         body = 'Use that code: ' + code + " to restore your password."
+#         letter = f"""\
+#         From: {email}
+#         To: {dest_email}
+#         Subject: {subject}
+#         Content-Type: text/plain; charset="UTF-8";
+#         {body}"""
+#         letter = letter.encode("UTF-8")
+#
+#         self.server.login(email, smtp_password)
+#         self.server.sendmail(email, dest_email, letter)
+#         self.server.quit()
+#
+#
+# email_service = EmailService
